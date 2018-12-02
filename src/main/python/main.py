cac315eb07f5eb8290acd9264cb12609eccd6b8f
Ivan Podkidyshev <raashicat@gmail.com>
2018-12-02 18:39:35 +0400

    Set up working with QT Designer

--- src/main/python/main.py
+++ src/main/python/main.py
@@ -3,13 +3,25 @@ from PyQt5.QtWidgets import QMainWindow
 
 import sys
 
-class AppContext(ApplicationContext):           # 1. Subclass ApplicationContext
+import test
+
+
+class ExampleApp(QMainWindow, test.Ui_MainWindow):
+    def __init__(self):
+        # Это здесь нужно для доступа к переменным, методам
+        # и т.д. в файле design.py
+        super().__init__()
+        self.setupUi(self)  # Это нужно для инициализации нашего дизайна
+
+
+class AppContext(ApplicationContext, test.Ui_MainWindow):           # 1. Subclass ApplicationContext
     def run(self):                              # 2. Implement run()
-        window = QMainWindow()
+        window = ExampleApp()
         window.setWindowTitle("Health-weather correlation")
-        window.resize(250, 150)
+        # window.resize(250, 150)
         window.show()
         return self.app.exec_()                 # 3. End run() with this line
+
 
 if __name__ == '__main__':
     appctxt = AppContext()                      # 4. Instantiate the subclass
