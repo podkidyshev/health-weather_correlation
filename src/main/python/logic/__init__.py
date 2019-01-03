d4d7098a66c9ad0c30009a5ce09cc76f80671842
Ivan Podkidyshev <raashicat@gmail.com>
2019-01-03 23:50:20 +0400

    Removed PIL from project

--- src/main/python/logic/__init__.py
+++ src/main/python/logic/__init__.py
@@ -1,10 +1,8 @@
 import os
 import sys
 
-from PIL.ImageQt import ImageQt
-
 from PyQt5.QtCore import QSize, Qt
-from PyQt5.QtGui import QPixmap, QImage
+from PyQt5.QtGui import QPixmap
 from PyQt5.QtWidgets import QFrame, QFileDialog
 
 
@@ -40,26 +38,24 @@ class QFrameBase(QFrame):
         self.setMinimumSize(QSize(500, 250))
         self.layout().setContentsMargins(0, 0, 0, 0)
 
-    def add_image(self, img_obj, img_label, canvas_name, **kwargs):
+    def add_image(self, img_obj, img_label, img_name):
         """
         принимает PIL-изображение и рисует на img-label картинку:
         добавляет необходимые объекты в корневой объект и конфигурирует лейбл
         :param img_obj: PIL-изображение для рисование
         :param img_label: лейбл, на котором рисовать
         :param canvas_name: имя объекта канваса - должно быть уникальным
-        :param kwargs: на всякий случай
         :return: None
         """
-        img_name = canvas_name + 'img'
+        if img_name in self.__dict__:
+            raise QFrameBase.QFrameBaseException('img_name должно быть уникальным')
 
-        if img_name in self.__dict__ or canvas_name in self.__dict__:
-            raise QFrameBase.QFrameBaseException('img_name и canvas_name должны быть уникальны')
-
-        self.__dict__[img_name] = QImage(ImageQt(img_obj))
-        self.__dict__[canvas_name] = QPixmap.fromImage(self.__dict__[img_name])
+        pixmap = QPixmap()
+        pixmap.loadFromData(img_obj)
+        self.__dict__[img_name] = pixmap
 
         img_label.installEventFilter(main_window)
-        img_label._pixmap = self.__dict__[canvas_name]
-        img_label.setPixmap(self.__dict__[canvas_name])
+        img_label._pixmap = pixmap
+        # img_label.setPixmap(pixmap)
         img_label.setAlignment(Qt.AlignCenter)
         img_label.setMinimumSize(QSize(200, 200))
